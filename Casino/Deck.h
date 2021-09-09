2506c35a28cdf5da329702dc4996473e31bd0f54
craughtigan <craughtigan@gmail.com>
2021-09-08 23:23:07 -0400

    Adding functions to Deck

--- Casino/Deck.h
+++ Casino/Deck.h
@@ -4,9 +4,17 @@
 
 class Deck {
 public:
-	// function to remove the top card from the deck and return in
+	// function to remove the top card from the deck and return it
 	Card getTopCard() {
 		
+	}
+	// function to remove the bottom card from the deck and return it
+	Card getBottomCard() {
+
+	}
+	// function to remove a random card from the deck and return it
+	Card getRandomCard() {
+
 	}
 	// function to add a card to the top of the deck
 	void addCardOnTop(Card card) {
@@ -14,6 +22,18 @@ public:
 	}
 	// function to add a card to the bottom of the deck
 	void addCardOnBottom(Card card) {
+
+	}
+	// create a deck of cards in a random order
+	void shuffle() {
+
+	}
+	// create a deck of cards in order
+	void order() {
+
+	}
+	// return the amount of cards remaining
+	int countRemainingCards() {
 
 	}
 };
